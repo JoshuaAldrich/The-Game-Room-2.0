375297b7d7e02ed2bf83f34bff1943a296e7f484
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-11-09 15:59:26 -0500

    Finished Register button

--- the-game-room-2.0/src/components/Register.js
+++ the-game-room-2.0/src/components/Register.js
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import {Navigate} from 'react-router-dom';
+import axios from "axios";
 
 export const Register = () => {
     const [name, setName] = useState('');
@@ -12,11 +13,38 @@ export const Register = () => {
 
         await axios.post('register', {
             name, email, password
-        });
-
-        setNavigate(true);
+        }
+    );
+    setNavigate(true);
     }
 
     if (navigate) {
-        return <Navigate to="/login"/>;
+        return <Navigate to="/Login"/>;
     }
+    return <main className="form-signin">
+        <form onSubmit={submit}>
+            <h1 className="h4 mb-3 fw-bold">Register Here</h1>
+            <div className="form-floating">
+                <input className="form-control" placeholder="Place Name"
+                       onChange={e => setName(e.target.value)}
+                />
+                <label>Name
+                </label>
+            </div>
+            <div className="form-floating">
+                <input type="email" className="form-control" id="floatingInput" placeholder="Email"
+                       onChange={e => setEmail(e.target.value)}
+                />
+                <label htmlFor="floatingInput">Email</label>
+            </div>
+
+            <div className="form-floating">
+                <input type="password" className="form-control" id="floatingPassword" placeholder="Password"
+                       onChange={e => setPassword(e.target.value)}
+                />
+                <label htmlFor="floatingPassword">Password</label>
+            </div>
+            <button className="w-200 btn btn-primary" type="submit">Submit</button>
+        </form>
+    </main>
+}
