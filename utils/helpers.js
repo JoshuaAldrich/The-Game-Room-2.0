29d8c672f4f49c46337802c49a52d1fde97c03fa
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-11-09 08:25:14 -0500

    creating utils for log in

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,20 @@
-function format_plural(noun, amount) {
-  if (amount !== 1) {
-    noun += "s";
-  }
-  return noun;
-}
+module.exports = {
+  format_date: date => {
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+  },
+  format_url: url => {
+      return url
+          .replace('http://', '')
+          .replace('https://', '')
+          .replace('www.', '')
+          .split('/')[0]
+          .split('?')[0];
+  },
+  format_plural: (word, amount) => {
+      if (amount !== 1) {
+          return `${word}s`;
+      }
 
-module.exports = { format_plural };
+      return word;
+  }
+};
