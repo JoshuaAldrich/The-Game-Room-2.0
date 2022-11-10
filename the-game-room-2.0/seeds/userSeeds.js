ba0a33483ddc5b3c27fc3c3f63156246df8cff62
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-11-09 20:14:38 -0500

    userseeds

---
+++ the-game-room-2.0/seeds/userSeeds.js
@@ -0,0 +1,9 @@
+const { User } = require("../models");
+const userSeeds = [
+  {  
+  },
+];
+
+const seededUsers = () => User.bulkCreate(userSeeds, { individualHooks: true });
+
+module.exports = seededUsers;
