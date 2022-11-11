26be6674a66b74e19362c507e77c77fa8e81a0a4
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-11-10 19:44:45 -0500

    Changes made to server.js and game.js to try and convert to mongodb

---
+++ the-game-room-2.0/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const userRoutes = require("./routes/user");
+const thoughtRoutes = require("./routes/thought");
+const app = express();
+const mongoose = require("mongoose");
+
+app.use(express.json());
+app.use("/api/thoughts", thoughtRoutes);
+app.use("/api/users", userRoutes);
+
+mongoose
+  .connect(
+    "mongodb+srv://joshuaA:abcd1234@cluster0.bnsumjl.mongodb.net/The-Game-Room?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(3001);
+  });
