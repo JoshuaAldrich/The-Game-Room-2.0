13d18dab846cae82ac939261c2a857dcf4fc8506
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-11-12 09:05:20 -0500

    trying to update heads or tails js

--- src/components/headsOrTails.js
+++ src/components/headsOrTails.js
@@ -1,11 +1,13 @@
+import headImg from "../images/heads.png";
+
 const HoT = () => {
   return (
     <div class="container-fluid">
       <h2 class="special-card-4">Heads or Tails</h2>
-
+      <div className="imageContainer">{headImg}</div>
       <button>Heads</button>
       <button>Tails</button>
-      <div></div>
+      <div>Result</div>
       <p>UserID</p>
       <p>Here is filler text</p>
     </div>
