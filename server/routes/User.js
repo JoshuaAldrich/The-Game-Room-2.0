79b95e6174b7fac5c37686e14863bc668b8d549b
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-11-12 13:08:18 -0500

    The ability to see current users signed in

--- server/routes/User.js
+++ server/routes/User.js
@@ -35,9 +35,48 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-//get one users stats
+//sign in the user
+router.post("/login", async (req, res) => {
+  try {
+    let user = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    });
 
-//post user stats
+    if (!user) {
+      res.status(422).json("Wrong email or password");
+      return;
+    }
+
+    let token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      "abc123"
+    );
+
+    res.json({
+      user,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json("An error has occurred");
+  }
+});
+
+//get the currently logged in users
+router.get("/currentUser", async (req, res) => {
+  try {
+    let token = req.get("Authorization");
+    let tokenData = jwt.verify(token, "abc123");
+    let user = await User.findOne({
+      _id: tokenData.userId,
+    });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json("An error has occurred");
+  }
+});
 
 //put user stats
 
