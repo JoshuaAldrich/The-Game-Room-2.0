dd9ce988ea0d0beef862c4e74c4bd51c79e20256
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-11-12 14:11:36 -0500

    reorganizing file structure

--- app-game-room/src/App.js
+++ app-game-room/src/App.js
@@ -1,18 +1,35 @@
+import HoT from "./components/headsOrTails";
+import HoL from "./components/highOrLow";
+import Auth from "./components/Auth";
+import RPS from "./components/rockPaperScissors";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
-import Homepage from './components/Homepage/Homepage';
-import Header from './components/Nav/Header';
+const App = () => {
+  let [user, setUser] = useState(null);
 
-
-function App() {
   return (
-    <div>
-      <Header />
-      <div>
-        <Homepage />
+    <>
+      <Nav user={user} />
 
+      <div className="pageContainer">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth setUser={setUser} />} />
+          <Route path="/RPS" element={<RPS />} />
+          <Route path="/HoL" element={<HoL />} />
+          <Route path="/HoT" element={<HoT />} />
+        </Routes>
       </div>
-    </div>
+      <article id="contact" class="contact-me">
+        <div>
+          <h2>The Game Room</h2>
+        </div>
+      </article>
+    </>
   );
-}
+};
 
 export default App;
