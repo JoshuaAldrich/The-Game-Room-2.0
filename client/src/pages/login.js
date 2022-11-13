1c383976edcc850dd5d7b147cef059a25f018209
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-11-13 16:07:54 -0500

    conecting login functionality, and some css as well

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from "../Utils/mutations";
 
 const Login = () => {
+    const [formstate, setFormState] = useState({ name: '', password: '' });
+    
+
+    const handleFormSubmit = async (event) => {
+        event.prevenDefault();
+    }
     return (
         <div className="flex-row justify-center mt-4">
             <div className="col-12">
+                <form onSubmit={handleFormSubmit}>
+                    <div className="form-group">
+                        <label for="exampleInputName1">Name</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            placeholder="Name" />
 
-                <div className="form-group">
-                    <label for="exampleInputEmail1">Name</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Name" />
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            placeholder="Password" />
+                    </div>
 
-                </div>
-                <div className="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-                </div>
-
-                <button type="submit" className="btn btn-primary mt-3">Submit</button>
+                    <button type="submit" className="btn btn-primary mt-3">Submit</button>
+                </form>
 
             </div>
 
