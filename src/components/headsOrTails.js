8a4789cbdca3b9a0e9befc361ef848795a9d8756
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-11-12 21:52:34 -0500

    Changes made to get heads or tails working

--- src/components/headsOrTails.js
+++ src/components/headsOrTails.js
@@ -1,71 +1,79 @@
-import headImg from "../images/heads.png";
+import headsImg from "../images/heads.png";
 import tailsImg from "../images/tails.png";
 import classes from "../styles/headsOrTails.module.css";
 import React, { useState } from "react";
 
 const HoT = () => {
-  // const [result, setResult] = useState("");
-
-  // async function heads() {
-  //   let coin = Math.random();
-  //   headImg.classList.toggle("rotate");
-
-  //   if (coin < 0.5) {
-  //     setTimeout(function () {
-  //       imageContainer = { tailsImg };
-  //       result.innerHTML =
-  //         "<span style='color:black;'>You chose incorrectly, it is tails</span>";
-  //     }, 1000);
-  //   } else {
-  //     setTimeout(function () {
-  //       imageContainer.innerHTML = `<img src="${headsImage}"/>`;
-  //       result.innerHTML =
-  //         "<span style='color:black;'>You chose correctly, it is heads</span>";
-  //     }, 1000);
-  //     const res = await fetch(`/api/users/win-HoT/${user_id}`, {
-  //       method: "PUT",
-  //     });
-  //     console.log(res);
-  //   }
-  // }
-
-  // async function tails() {
-  //   let coin = Math.random();
-  //   imageContainer.firstElementChild.classList.toggle("rotate");
-
-  //   if (coin < 0.5) {
-  //     setTimeout(function () {
-  //       imageContainer.innerHTML = `<img src="${tailsImage}"/>`;
-  //       result.innerHTML =
-  //         "<span style='color:black;'>You chose correctly, it is heads</span>";
-  //     }, 1000);
-  //     const res = await fetch(`/api/users/win-HoT/${user_id}`, {
-  //       method: "PUT",
-  //     });
-  //     console.log(res);
-  //   } else {
-  //     setTimeout(function () {
-  //       imageContainer.innerHTML = `<img src="${headsImage}"/>`;
-  //       result.innerHTML =
-  //         "<span style='color:black;'>You chose incorrectly, it is tails</span>";
-  //     }, 1000);
-  //   }
-  // }
+  const [result, setResult] = useState("");
+  async function heads() {
+    let coin = Math.random();
+    document.querySelector("#imageContainer").classList.toggle(classes.rotate);
+    if (coin < 0.5) {
+      setTimeout(function () {
+        setResult({
+          img: tailsImg,
+          message: (
+            <span style={{ color: "black" }}>
+              You chose incorrectly, it is tails
+            </span>
+          ),
+        });
+      }, 1000);
+    } else {
+      setTimeout(function () {
+        setResult({
+          img: headsImg,
+          message: (
+            <span style={{ color: "black" }}>
+              You chose correctly, it is heads
+            </span>
+          ),
+        });
+      }, 1000);
+    }
+  }
+  async function tails() {
+    let coin = Math.random();
+    document.querySelector("#imageContainer").classList.toggle(classes.rotate);
+    if (coin < 0.5) {
+      setTimeout(function () {
+        setResult({
+          img: headsImg,
+          message: (
+            <span style={{ color: "black" }}>
+              You chose correctly, it is tails
+            </span>
+          ),
+        });
+      }, 1000);
+    } else {
+      setTimeout(function () {
+        setResult({
+          img: tailsImg,
+          message: (
+            <span style={{ color: "black" }}>
+              You chose incorrectly, it is heads
+            </span>
+          ),
+        });
+      }, 1000);
+    }
+  }
 
   return (
     <div>
       <h2>Heads or Tails</h2>
-      {/* <img
+      <img
         id="imageContainer"
-        src={headImg}
+        src={result?.img ? result.img : headsImg}
         alt="Head coin"
         className={classes.imageContainer}
       />
       <button onClick={heads}>Heads</button>
       <button onClick={tails}>Tails</button>
-      <div id="result">Result</div>
+      <div>{result?.message}</div>
       <p className="user_id">UserID</p>
-      <p>Here is filler text</p> */}
+      <p>Here is filler text</p>
     </div>
   );
 };
