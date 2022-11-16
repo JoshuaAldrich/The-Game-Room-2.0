7b120b92ec88a73dfd2d5493970cd593ce767fdd
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-15 19:54:23 -0500

    started hangman

--- server/server.js
+++ server/server.js
@@ -23,13 +23,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
