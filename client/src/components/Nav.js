8dca1c1ec0efcf9e4325dc0f571aaf58d5019700
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-11-15 20:35:29 -0500

    Cant get rock paper scissors logic to work

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,10 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import auth from "../Utils/auth";
 
-
 const Nav = () => {
   return (
-
     <header className="bg-dark text-white py-2 flex-row align-center ">
       <div className=" container d-flex justify-content-around ">
         <Link to="/">
@@ -16,31 +14,24 @@ const Nav = () => {
             <>
               <ul>
                 <p>YOU ARE LOGGED IN!</p>
-                <Link to="/HoL"
-                  className="px-2">High or Low
+                <Link to="/HoL" className="px-2">
+                  High or Low
                 </Link>
-
               </ul>
             </>
-
           ) : (
             <ul>
-              <Link to="/login"
-                className="px-2">Login
+              <Link to="/login" className="px-2">
+                Login
               </Link>
-              <Link to="/signup"
-                className="px-2">Sign Up
+              <Link to="/signup" className="px-2">
+                Sign Up
               </Link>
             </ul>
-
           )}
-
         </div>
-
-
       </div>
-
     </header>
-  )
+  );
 };
 export default Nav;
--- client/src/components/rockPaperScissors.js
+++ client/src/components/rockPaperScissors.js
@@ -1,22 +1,99 @@
+import rockImage from "../images/Rock.png";
+import scissorImage from "../images/scissors.png";
+import paperImage from "../images/paper.png";
+import React, { useState } from "react";
+
 const RPS = () => {
-  return (
-    <div>
-      <h2>Rock Paper Scissors</h2>
-      <h2>
-        Your Choice: <span></span>
-      </h2>
-      <h2>
-        Computer Choice: <span></span>
-      </h2>
-      <h2>
-        Winner: <span></span>
-      </h2>
-      <button>Rock</button>
-      <button>Paper</button>
-      <button>Scissors</button>
-      <p></p>
-    </div>
-  );
+  // const [result, setResult] = useState("");
+  // const Rock = () => {
+  //   let userChoice = "Rock";
+  //   generateComputerChoice();
+  //   getResult();
+  //   function generateComputerChoice() {
+  //     const randomNumber = Math.floor(Math.random() * 3) + 1;
+  //     if (randomNumber === 1) {
+  //       computerChoice = Rock;
+  //     }
+  //     if (randomNumber === 2) {
+  //       computerChoice = Scissor;
+  //     }
+  //     if (randomNumber === 3) {
+  //       computerChoice = Paper;
+  //     }
+  //   }
+  //   async function getResult() {
+  //     if (computerChoice === userChoice) {
+  //       result = "Draw!";
+  //     }
+  //     if (computerChoice === Rock && userChoice === Paper) {
+  //       console.log(res);
+  //       result = "Username!";
+  //     }
+  //     if (computerChoice === Rock && userChoice === Scissor) {
+  //       result = "Computer!";
+  //     }
+  //     if (computerChoice === Paper && userChoice === Scissor) {
+  //       console.log(res);
+  //       result = "Username!";
+  //     }
+  //     if (computerChoice === Paper && userChoice === Scissor) {
+  //       result = "Computer!";
+  //     }
+  //     if (computerChoice === Scissor && userChoice === Rock) {
+  //       result = "Username!";
+  //     }
+  //     if (computerChoice === Scissor && userChoice === Paper) {
+  //       result = "Computer!";
+  //     }
+  //     resultDisplay = result;
+  //   }
+  // };
+  // possibleChoices.forEach((possibleChoice) =>
+  //   possibleChoice.addEventListener("click", (e) => {
+  //     userChoice = e.target.id;
+  //     if (userChoice === "Rock") {
+  //       userChoiceImage = rockImage;
+  //     }
+  //     if (userChoice === "Scissors") {
+  //       userChoiceImage = scissorImage;
+  //     }
+  //     if (userChoice === "Paper") {
+  //       userChoiceImage = paperImage;
+  //     }
+  //     generateComputerChoice();
+  //     getResult();
+  //   })
+  // );
+  // function generateComputerChoice() {
+  //   const randomNumber = Math.floor(Math.random() * 3) + 1;
+  //   if (randomNumber === 1) {
+  //     computerChoice = rockImage;
+  //   }
+  //   if (randomNumber === 2) {
+  //     computerChoice = scissorImage;
+  //   }
+  //   if (randomNumber === 3) {
+  //     computerChoice = paperImage;
+  //   }
+  // }
+  // return (
+  //   <div>
+  //     <h2>Rock Paper Scissors</h2>
+  //     <h2>
+  //       Your Choice: <span>{userChoiceImage}</span>
+  //     </h2>
+  //     <h2>
+  //       Computer Choice: <span>{computerChoice}</span>
+  //     </h2>
+  //     <h2>
+  //       Winner: <span>{Winner}</span>
+  //     </h2>
+  //     <button onClick={Rock}>Rock</button>
+  //     <button onClick={Paper}>Paper</button>
+  //     <button onClick={Scissors}>Scissors</button>
+  //     <p>{result}</p>
+  //   </div>
+  // );
 };
 
 export default RPS;
