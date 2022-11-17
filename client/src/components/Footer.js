f2d0ce2541de758e9f19aa7ea8d8974ceb001321
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-11-17 11:28:45 -0500

    adding css

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Footer = () => 
-<div className="page-footer font-small blue p-3">
+<div className="footer page-footer font-small blue p-3">
             <div className="bg-dark border border-light rounded text-center text-light border border-3">
                 <div className="text-light bg-danger text-center p-2 m-2 border border-light rounded  border border-2">Created by:</div>
                 <p>Los Afortuandos</p>
