07b05b783b79a0e368885b0f5cc1d62a6f52e6d3
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-11-17 22:38:34 -0500

    changes to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,49 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import NavBar from "./components/NavBar"
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import NavBar from "./components/NavBar";
 import HoT from "./components/headsOrTails";
-import HoL from './pages/HoL';
+import HoL from "./pages/HoL";
 import RPS from "./components/rockPaperScissors";
 import Home from "./pages/Home";
-import Login from "./pages/Login"
-import Footer from "./components/Footer"
+import Login from "./pages/login";
+import Footer from "./components/Footer";
 // import Nav from "./components/Nav";
 // import { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Signup from "./pages/Signup";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `${token}` : '',
-    }
-  }
-})
+      authorization: token ? `${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
-
   return (
     <ApolloProvider client={client}>
-
       <div className="flex-column justify-flex-start min-100-vh bg-dark">
-        <NavBar /> 
+        <NavBar />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -53,7 +56,6 @@ const App = () => {
           <Footer />
         </div>
       </div>
-
     </ApolloProvider>
   );
 };
